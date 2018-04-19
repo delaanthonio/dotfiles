9758745636e9536702ddb025356d9e948b2d886c
Dela Anthonio <dell.anthonio@gmail.com>
2018-04-19 16:32:09 -0400

    init.el: Set default font size to 14

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -155,7 +155,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro for Powerline"
-                               :size 15
+                               :size 14
                                :weight normal
                                :width normal
                                :powerline-scale 1.2)
