1fbfe7e6ab9fee57c5664871599c9ec6c04418db
Dela Anthonio <dell.anthonio@gmail.com>
2018-11-13 20:26:07 -0500

    [spacemacs] Change default font to Iosevka Term

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -216,7 +216,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Iosevka"
+   dotspacemacs-default-font '("Iosevka Term"
                                :size 14
                                :weight medium
                                :width normal
