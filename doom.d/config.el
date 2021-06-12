d882029d343b4877bc20ba233bd1f54818cc798a
Dela Anthonio <dell@anthonio@gmail.com>
2021-06-12 16:20:08 -0400

    use shell path as emacs path

--- doom.d/config.el
+++ doom.d/config.el
@@ -105,6 +105,22 @@
 (add-to-list 'auto-mode-alist '("zprofile\\'" . sh-mode) t)
 (add-to-list 'auto-mode-alist '("\\.zshenv_local\\'" . sh-mode) t)
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$" "" (shell-command-to-string
+                                          "$SHELL --login -c 'echo $PATH'"
+                                          ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 ;; Enable plantuml-mode for PlantUML files
 (after! plantuml-mode
   :config
