04f12518c1873ff8d0936537eafe7b55145cb620
Dela Anthonio <dell@anthonio@gmail.com>
2021-06-18 09:32:17 -0400

    disable solaire mode

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -41,6 +41,7 @@
 ;; (package! builtin-package :pin "1a2b3c4d5e")
 
 (package! visual-fill-column)
+(package! solaire-mode :disable t)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
