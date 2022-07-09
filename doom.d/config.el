bd19a30872f14364b19213ac0fba3b170606802c
Dela Anthonio <dell@anthonio@gmail.com>
2022-07-09 13:02:40 -0400

    [feat] conditionally set default org directory

--- doom.d/config.el
+++ doom.d/config.el
@@ -130,7 +130,9 @@ https://code.orgmode.org/bzg/org-mode/commit/13424336a6f30c50952d291e7a82906c121
 
 ;;; Org
 (after! org
-  (setq org-directory "~/Dropbox/Org/")
+  (setq org-directory
+        (cond ((file-directory-p (expand-file-name "~/Dropbox/Org")) "~/Dropbox/Org")
+              (t "~/Org")))
 
   (setq org-agenda-files (directory-files org-directory t "\\.org$" t)
         org-archive-location "::* Archive"
