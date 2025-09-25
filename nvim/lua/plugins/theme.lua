2daded861daa3eb8427d04f34922ad037f021535
Dela Anthonio <dela@anthonio.pro>
2025-09-25 17:03:23 -0400

    [feat] add gruvbox theme for neovim

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- set LazyVim's default colorscheme to gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
