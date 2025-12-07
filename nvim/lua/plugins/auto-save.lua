deb1308ac6aee7b63434761d790508d9a90e5718
Dela Anthonio <dela@anthonio.pro>
2025-12-07 16:16:32 -0500

    fix(nvim): auto-save

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -5,14 +5,6 @@ return {
 		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
 		opts = {
 			enabled = true,
-			execution_message = {
-				enabled = true,
-				message = function()
-					return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-				end,
-				dim = 0.18,
-				cleaning_interval = 1250,
-			},
 			trigger_events = {
 				immediate_save = { "BufLeave", "FocusLost" },
 				defer_save = { "InsertLeave", "TextChanged" },
