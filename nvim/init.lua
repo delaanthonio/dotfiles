9a0bf1cfdb19e8d0d65f5ce1b70affe19008e40d
Dela Anthonio <dela.anthonio@agendacraft.ai>
2026-01-14 15:08:59 -0500

    feat: allow using nerd font

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
+-- Detect if a Nerd Font is being used
+-- Since you have Symbols Nerd Font installed and using kitty terminal
+-- Set this to true to use Nerd Font icons in UI
+vim.g.have_nerd_font = true
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
