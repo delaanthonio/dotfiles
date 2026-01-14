4f4f877ceafa4da7ab905e831f8132b6b4709f9b
Dela Anthonio <dela.anthonio@agendacraft.ai>
2026-01-14 15:09:18 -0500

    fix(nvim): snacks setup

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,14 +5,9 @@ return {
   opts = {
     -- Core components
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
     indent = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    picker = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
 
