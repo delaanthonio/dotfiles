0ccbd8db42ce389fbc95ce0d21a7652e0f693b4b
Dela Anthonio <dela.anthonio@agendacraft.ai>
2026-01-29 17:57:39 -0500

    feat(nvim): enhance oil setup

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,7 +1,117 @@
 return {
   "stevearc/oil.nvim",
-  --@module "oil"
-  --@type oil.SetupOts
-  opts = {},
-  dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+      default_file_explorer = true,
+
+      -- Columns to display (similar to dired details)
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+      },
+
+      -- Buffer-local options to use for oil buffers
+      buf_options = {
+        buflisted = false,
+        bufhidden = "hide",
+      },
+
+      -- Window-local options to use for oil buffers
+      win_options = {
+        wrap = false,
+        signcolumn = "no",
+        cursorcolumn = false,
+        foldcolumn = "0",
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = "nvic",
+      },
+
+      -- Delete to trash instead of permanently deleting
+      delete_to_trash = true,
+
+      -- Skip confirmation for simple operations
+      skip_confirm_for_simple_edits = true,
+
+      -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+      prompt_save_on_select_new_entry = true,
+
+      -- Oil will automatically delete hidden buffers after this delay
+      cleanup_delay_ms = 2000,
+
+      -- Keymaps in oil buffer (dired-like bindings)
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open in vertical split" },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open in horizontal split" },
+        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open in new tab" },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = "cd to directory in current tab" },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = false,
+
+      -- Configuration for the floating window
+      float = {
+        padding = 2,
+        max_width = 90,
+        max_height = 30,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+
+      -- Configuration for the actions floating preview window
+      preview = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = 0.9,
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+
+      -- Configuration for the floating progress window
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = "rounded",
+        minimized_border = "none",
+        win_options = {
+          winblend = 0,
+        },
+      },
+    })
+
+    -- Keymap to open oil in the current directory
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+    -- Keymap to open oil in a floating window
+    vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open oil in floating window" })
+  end,
 }
