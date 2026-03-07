ec0caf3ee20df98cd9e5f6801d9ad84c938da9b4
Dela Anthonio <dela.anthonio@agendacraft.ai>
2026-03-07 12:20:22 -0500

    feat(fish): add brew config

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,7 +1,11 @@
-if test -d (brew --prefix)"/share/fish/completions"
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
-end
+if test -x /opt/homebrew/bin/brew
+    set -gx PATH /opt/homebrew/bin $PATH
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
 end
