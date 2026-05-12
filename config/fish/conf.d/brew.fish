594dc38457af036e1cac5c1334b4130dac9d095b
Dela Anthonio <dela@anthonio.pro>
2026-05-11 21:17:26 -0400

    chore: update conditionally loading brew

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,11 +1,3 @@
 if test -x /opt/homebrew/bin/brew
-    set -gx PATH /opt/homebrew/bin $PATH
-
-    if test -d (brew --prefix)"/share/fish/completions"
-        set -p fish_complete_path (brew --prefix)/share/fish/completions
-    end
-
-    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-    end
+    eval (/opt/homebrew/bin/brew shellenv)
 end
